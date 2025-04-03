e23b6b4c0f60d8869139c2c94cc23425cdd69348
nayannelbatista <nayannelopes.bd@gmail.com>
2025-04-03 18:23:11 -0300

    refatoração de função

--- src/app/interceptors/erro.interceptor.ts
+++ src/app/interceptors/erro.interceptor.ts
@@ -17,11 +17,14 @@ export const erroInterceptor: HttpInterceptorFn = (req, next) => {
   )
 };
 
-function obterMensagemDeErro(status: number): string {
-  const mensagensDeErro: Record<number, string> = {
-    0: 'Erro de rede - Não foi possível se conectar ao servidor.',
-    404: 'O recurso solicitado não foi encontrado.',
-    500: 'Erro no servidor. Tente novamente mais tarde.'
-  }
-  return mensagensDeErro[status] || 'Ocorreu um erro inesperado'
+export const MENSAGENS_ERRO: Record<number, string> = {
+  0: 'Erro de rede - Não foi possível se conectar ao servidor.',
+  401: 'Não autorizado. Faça login novamente.',
+  403: 'Acesso proibido. Você não tem permissão para esta ação.',
+  404: 'O recurso solicitado não foi encontrado.',
+  500: 'Erro no servidor. Tente novamente mais tarde.'
+};
+
+export function obterMensagemDeErro(status: number): string {
+  return MENSAGENS_ERRO[status] || 'Ocorreu um erro inesperado';
 }
